e7c5485e786d7bc6ea7c086d1708f76e56147d39
Andrea F. Daniele <afdaniele@ttic.edu>
2018-08-14 13:54:38 -0500

    added ros-system-monitor (4 variants)

--- ros-system-monitor/standalone/assets/launch_system_monitor.sh
+++ ros-system-monitor/standalone/assets/launch_system_monitor.sh
@@ -3,4 +3,4 @@
 git -C /home/software/catkin_ws/src/ros-system-monitor pull origin master
 
 source /home/software/catkin_ws/devel/setup.bash
-roslaunch /system_monitor.launch
+roslaunch /webserver_sysmonitor.launch

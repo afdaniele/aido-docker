82b09ce9eb00111cbdc2ebd353c780b225975b46
Andrea F. Daniele <afdaniele@ttic.edu>
2018-08-17 12:40:13 -0500

    fixed bug

--- rpi-blockly-duckiebot-frontend/assets/entrypoint_blockly_frontend.sh
+++ rpi-blockly-duckiebot-frontend/assets/entrypoint_blockly_frontend.sh
@@ -3,15 +3,6 @@
 # constants
 HOSTNAME=$(hostname)
 
-# install/update compose
-/root/install_compose.sh
-
-# check volume
-mountpoint -q $COMPOSE_DIR
-if [ $? -ne 0 ]; then
-    echo "WARNING: The path '$COMPOSE_DIR' is neither a VOLUME nor a MOUNTPOINT. All the changes will be deleted with the container."
-fi
-
 # clone or update the `duckietown` package
 git -C $COMPOSE_PACKAGES_DIR/duckietown pull origin master || git clone --depth 1 https://github.com/afdaniele/compose-pkg-duckietown $COMPOSE_PACKAGES_DIR/duckietown
 
